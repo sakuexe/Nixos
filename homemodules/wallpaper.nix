1ac87e94592c85d71313bd79af2c89125e567732
Saku Karttunen <saku.karttunen@gmail.com>
2025-01-05 09:25:27 +0200

    reformatted wallpaper.nix

--- homemodules/wallpaper.nix
+++ homemodules/wallpaper.nix
@@ -1,12 +1,17 @@
-{ config, lib, home-manager, ... }: 
-  let
-    hasPlasmaManager = builtins.hasAttr "plasma" config.programs;
-    wallpaperPath = config.xdg.configHome;
-  in {
+{
+  config,
+  lib,
+  home-manager,
+  ...
+}:
+let
+  hasPlasmaManager = builtins.hasAttr "plasma" config.programs;
+  wallpaperPath = config.xdg.configHome;
+in
+{
 
   options.wallpaper = {
-    enable = 
-      lib.mkEnableOption "enables the wallpaper module";
+    enable = lib.mkEnableOption "enables the wallpaper module";
 
     ultrawide = lib.mkEnableOption {
       type = lib.types.bool;
@@ -24,7 +29,11 @@
       type = lib.types.float;
       default = 0.75;
       description = "Opacity of the background image. The value is between 0 and 1.";
-      apply = value: lib.throwIf (value < 0 || value > 1) "wallpaper.backgroundImageOpacity value should be between 0 and 1" value;
+      apply =
+        value:
+        lib.throwIf (
+          value < 0 || value > 1
+        ) "wallpaper.backgroundImageOpacity value should be between 0 and 1" value;
     };
 
     backgroundColor = lib.mkOption {
@@ -37,7 +46,8 @@
       type = lib.types.float;
       default = 1;
       description = "The scale of the icon if there is one";
-      apply = value: lib.throwIf (value < 0) "wallpaper.iconScale value should be a positive float." value;
+      apply =
+        value: lib.throwIf (value < 0) "wallpaper.iconScale value should be a positive float." value;
     };
   };
 
@@ -46,134 +56,144 @@
     # idea from:
     # https://github.com/lucidph3nx/nixos-config/blob/main/modules/home-manager/desktopEnvironment/wallpaper.nix
     home.file."${wallpaperPath}/nixos_logo.svg" = lib.mkIf (!config.wallpaper.ultrawide) {
-      text = let
-        bgImage = if (config.wallpaper.backgroundImage != null) then config.wallpaper.backgroundImage else "";
-        primaryColor = "#8255c2";
-        secondaryColor = "#b8a0d9";
-      in
-      # svg
-      ''
-      <?xml version="1.0" encoding="UTF-8"?>
-      <svg 
-        xmlns="http://www.w3.org/2000/svg"
-        width="2560"
-        height="1440"
-        version="1.1"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        viewBox="0 0 2560 1440">
+      text =
+        let
+          bgImage =
+            if (config.wallpaper.backgroundImage != null) then config.wallpaper.backgroundImage else "";
+          primaryColor = "#8255c2";
+          secondaryColor = "#b8a0d9";
+        in
+        # svg
+        ''
+          <?xml version="1.0" encoding="UTF-8"?>
+          <svg 
+            xmlns="http://www.w3.org/2000/svg"
+            width="2560"
+            height="1440"
+            version="1.1"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 2560 1440">
 
-        <rect id="background"
-          width="2560"
-          height="1440"
-          style="fill: ${config.wallpaper.backgroundColor}; stroke-width: 0px;" />
+            <rect id="background"
+              width="2560"
+              height="1440"
+              style="fill: ${config.wallpaper.backgroundColor}; stroke-width: 0px;" />
 
-        <image id="background-image" 
-          xlink:href="${bgImage}"
-          x="0" y="0" 
-          width="2560" height="1440"
-          preserveAspectRatio="xMidYMid slice"
-          style="opacity: ${toString config.wallpaper.backgroundImageOpacity};"/>
+            <image id="background-image" 
+              xlink:href="${bgImage}"
+              x="0" y="0" 
+              width="2560" height="1440"
+              preserveAspectRatio="xMidYMid slice"
+              style="opacity: ${toString config.wallpaper.backgroundImageOpacity};"/>
 
-        <g id="nixlogo" 
-          transform="scale(${toString config.wallpaper.iconScale})"
-          style="transform-origin: center; transform-box: border-box;">
-          <path 
-            id="part1"
-            d="M1384.84,714.4l-130.34-225.77,59.9-.56,34.8,60.66,35.04-60.33h29.76s15.24,26.35,15.24,26.35l-49.93,85.85,35.44,61.68-29.92,52.14Z" 
-            style="fill: ${secondaryColor};"/>
-          <path 
-            id="part2"
-            d="M1337.5,807.37l130.36-225.76,30.44,51.59-35.13,60.46,69.77.18,14.87,25.78-15.18,26.37-99.31-.31-35.69,61.53-60.11.16Z" 
-            style="fill: ${primaryColor};" />
-          <path 
-            id="part3"
-            d="M1233.11,813.33h260.69s-29.46,52.17-29.46,52.17l-69.93-.19,34.73,60.52-14.89,25.77-30.43.03-49.39-86.16-71.14-.14-30.19-51.98Z" 
-            style="fill: ${secondaryColor};" />
-          <path 
-            id="part4" 
-            d="M1176.06,725.86l130.34,225.77-59.9.56-34.8-60.66-35.04,60.33h-29.76s-15.24-26.35-15.24-26.35l49.93-85.85-35.44-61.68,29.92-52.14Z" 
-            style="fill: ${primaryColor};" />
-          <path 
-            id="part5" 
-            d="M1223.11,632.86l-130.36,225.76-30.44-51.59,35.13-60.46-69.77-.18-14.87-25.78,15.18-26.37,99.31.31,35.69-61.53,60.11-.16Z" 
-            style="fill: ${secondaryColor};" />
-          <path 
-            id="part6" 
-            d="M1327.26,626.41h-260.69s29.46-52.17,29.46-52.17l69.93.19-34.73-60.52,14.89-25.77,30.43-.03,49.39,86.16,71.14.14,30.19,51.98Z" 
-            style="fill: ${primaryColor};" />
-          </g>
-        </svg>
-      '';
+            <g id="nixlogo" 
+              transform="scale(${toString config.wallpaper.iconScale})"
+              style="transform-origin: center; transform-box: border-box;">
+              <path 
+                id="part1"
+                d="M1384.84,714.4l-130.34-225.77,59.9-.56,34.8,60.66,35.04-60.33h29.76s15.24,26.35,15.24,26.35l-49.93,85.85,35.44,61.68-29.92,52.14Z" 
+                style="fill: ${secondaryColor};"/>
+              <path 
+                id="part2"
+                d="M1337.5,807.37l130.36-225.76,30.44,51.59-35.13,60.46,69.77.18,14.87,25.78-15.18,26.37-99.31-.31-35.69,61.53-60.11.16Z" 
+                style="fill: ${primaryColor};" />
+              <path 
+                id="part3"
+                d="M1233.11,813.33h260.69s-29.46,52.17-29.46,52.17l-69.93-.19,34.73,60.52-14.89,25.77-30.43.03-49.39-86.16-71.14-.14-30.19-51.98Z" 
+                style="fill: ${secondaryColor};" />
+              <path 
+                id="part4" 
+                d="M1176.06,725.86l130.34,225.77-59.9.56-34.8-60.66-35.04,60.33h-29.76s-15.24-26.35-15.24-26.35l49.93-85.85-35.44-61.68,29.92-52.14Z" 
+                style="fill: ${primaryColor};" />
+              <path 
+                id="part5" 
+                d="M1223.11,632.86l-130.36,225.76-30.44-51.59,35.13-60.46-69.77-.18-14.87-25.78,15.18-26.37,99.31.31,35.69-61.53,60.11-.16Z" 
+                style="fill: ${secondaryColor};" />
+              <path 
+                id="part6" 
+                d="M1327.26,626.41h-260.69s29.46-52.17,29.46-52.17l69.93.19-34.73-60.52,14.89-25.77,30.43-.03,49.39,86.16,71.14.14,30.19,51.98Z" 
+                style="fill: ${primaryColor};" />
+              </g>
+            </svg>
+        '';
     };
 
     home.file."${wallpaperPath}/nixos_logo_ultrawide.svg" = lib.mkIf config.wallpaper.ultrawide {
-      text = let
-        bgImage = if (config.wallpaper.backgroundImage != null) then config.wallpaper.backgroundImage else "";
-        primaryColor = "#8255c2";
-        secondaryColor = "#b8a0d9";
-      in
-        /* svg */
+      text =
+        let
+          bgImage =
+            if (config.wallpaper.backgroundImage != null) then config.wallpaper.backgroundImage else "";
+          primaryColor = "#8255c2";
+          secondaryColor = "#b8a0d9";
+        in
+        # svg
         ''
-        <?xml version="1.0" encoding="UTF-8"?>
-        <svg 
-          xmlns="http://www.w3.org/2000/svg"
-          width="5120"
-          height="1440"
-          version="1.1"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          viewBox="0 0 5120 1440">
-
-          <rect 
-            id="background"
+          <?xml version="1.0" encoding="UTF-8"?>
+          <svg 
+            xmlns="http://www.w3.org/2000/svg"
             width="5120"
             height="1440"
-            style="fill: ${config.wallpaper.backgroundColor}; stroke-width: 0px;" />
+            version="1.1"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 5120 1440">
 
-          <image 
-            id="background-image" 
-            x="0" y="0" 
-            xlink:href="${bgImage}"
-            width="5120" height="1440"
-            preserveAspectRatio="xMidYMid slice"
-            style="opacity: ${toString config.wallpaper.backgroundImageOpacity};"/>
+            <rect 
+              id="background"
+              width="5120"
+              height="1440"
+              style="fill: ${config.wallpaper.backgroundColor}; stroke-width: 0px;" />
 
-          <g id="nixlogo">
-            <path 
-              id="part1"
-              d="M2664.77,713.65l-130.34-225.77,59.9-.56,34.8,60.66,35.04-60.33h29.76s15.24,26.35,15.24,26.35l-49.93,85.85,35.44,61.68-29.92,52.14Z" 
-              style="fill: ${primaryColor};" />
-            <path 
-              id="part2"
-              d="M2617.43,806.61l130.36-225.76,30.44,51.59-35.13,60.46,69.77.18,14.87,25.78-15.18,26.37-99.31-.31-35.69,61.53-60.11.16Z" 
-              style="fill: ${secondaryColor};" />
-            <path 
-              id="part3"
-              d="M2513.03,812.57h260.69s-29.46,52.17-29.46,52.17l-69.93-.19,34.73,60.52-14.89,25.77-30.43.03-49.39-86.16-71.14-.14-30.19-51.98Z" 
-              style="fill: ${primaryColor};" />
-            <path 
-              id="part4"
-              d="M2455.99,725.1l130.34,225.77-59.9.56-34.8-60.66-35.04,60.33h-29.76s-15.24-26.35-15.24-26.35l49.93-85.85-35.44-61.68,29.92-52.14Z"
-              style="fill: ${secondaryColor};" />
-            <path 
-              id="part5"
-              d="M2503.03,632.1l-130.36,225.76-30.44-51.59,35.13-60.46-69.77-.18-14.87-25.78,15.18-26.37,99.31.31,35.69-61.53,60.11-.16Z"
-              style="fill: ${primaryColor};" />
-            <path 
-              id="part6"
-              d="M2607.18,625.66h-260.69s29.46-52.17,29.46-52.17l69.93.19-34.73-60.52,14.89-25.77,30.43-.03,49.39,86.16,71.14.14,30.19,51.98Z"
-              style="fill: ${secondaryColor};" />
-            </g>
-          </svg>
+            <image 
+              id="background-image" 
+              x="0" y="0" 
+              xlink:href="${bgImage}"
+              width="5120" height="1440"
+              preserveAspectRatio="xMidYMid slice"
+              style="opacity: ${toString config.wallpaper.backgroundImageOpacity};"/>
+
+            <g id="nixlogo">
+              <path 
+                id="part1"
+                d="M2664.77,713.65l-130.34-225.77,59.9-.56,34.8,60.66,35.04-60.33h29.76s15.24,26.35,15.24,26.35l-49.93,85.85,35.44,61.68-29.92,52.14Z" 
+                style="fill: ${primaryColor};" />
+              <path 
+                id="part2"
+                d="M2617.43,806.61l130.36-225.76,30.44,51.59-35.13,60.46,69.77.18,14.87,25.78-15.18,26.37-99.31-.31-35.69,61.53-60.11.16Z" 
+                style="fill: ${secondaryColor};" />
+              <path 
+                id="part3"
+                d="M2513.03,812.57h260.69s-29.46,52.17-29.46,52.17l-69.93-.19,34.73,60.52-14.89,25.77-30.43.03-49.39-86.16-71.14-.14-30.19-51.98Z" 
+                style="fill: ${primaryColor};" />
+              <path 
+                id="part4"
+                d="M2455.99,725.1l130.34,225.77-59.9.56-34.8-60.66-35.04,60.33h-29.76s-15.24-26.35-15.24-26.35l49.93-85.85-35.44-61.68,29.92-52.14Z"
+                style="fill: ${secondaryColor};" />
+              <path 
+                id="part5"
+                d="M2503.03,632.1l-130.36,225.76-30.44-51.59,35.13-60.46-69.77-.18-14.87-25.78,15.18-26.37,99.31.31,35.69-61.53,60.11-.16Z"
+                style="fill: ${primaryColor};" />
+              <path 
+                id="part6"
+                d="M2607.18,625.66h-260.69s29.46-52.17,29.46-52.17l69.93.19-34.73-60.52,14.89-25.77,30.43-.03,49.39,86.16,71.14.14,30.19,51.98Z"
+                style="fill: ${secondaryColor};" />
+              </g>
+            </svg>
         '';
-      };
-
-      # add the wallpaper to kde if plasma manager exists
-      programs = if hasPlasmaManager then {
-        plasma.enable = true;
-        plasma.workspace.wallpaper = if config.wallpaper.ultrawide 
-          then ../assets/floating-cubes.jpg
-          else ../assets/dark_leaves_wp.jpg;
-        plasma.workspace.wallpaperBackground.blur = true;
-      } else {};
     };
-  }
+
+    # add the wallpaper to kde if plasma manager exists
+    programs =
+      if hasPlasmaManager then
+        {
+          plasma.enable = true;
+          plasma.workspace.wallpaper =
+            if config.wallpaper.ultrawide then
+              "${wallpaperPath}/nixos_logo_ultrawide.svg"
+            else
+              "${wallpaperPath}/nixos_logo.svg";
+          plasma.workspace.wallpaperBackground.blur = true;
+        }
+      else
+        { };
+  };
+}
