84d1268e2de34279237f6add292d2eec86fe2ce0
Saku Karttunen <saku.karttunen@gmail.com>
2025-05-10 12:52:05 +0300

    tweaked hyprland back to normal

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,36 +1,25 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  # create a seperate boot menu entry for hyprland
-  # so that it does not 
-  specialisation.hyprland.configuration = {
-    # optional: change the GRUB/systemd-boot name
-    system.nixos.tags = [ "hyprland" ];
-    system.nixos.label = "NixOS:Hyprland";
+  # enable hyprland
+  programs.hyprland.enable = true;
+  services.blueman.enable = true;
 
-    # enable hyprland
-    programs.hyprland.enable = true;
-    services.blueman.enable = true;
+  # related packages
+  environment.systemPackages = with pkgs; [
+    waybar # statusbar
+    rofi-wayland # launcher
+    fuzzel # other launcher (testing for now)
+    hyprpaper # wallpaper
+    kdePackages.dolphin
+  ];
 
-    # disable the KDE Plasma things.
-    services.desktopManager.plasma6.enable = lib.mkForce false;
-    # services.displayManager.sddm.enable = lib.mkOverride true;
+  # hint electron apps to use Wayland
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
-    # related packages
-    environment.systemPackages = with pkgs; [
-      waybar # statusbar
-      rofi-wayland # launcher
-      hyprpaper # wallpaper
-      kdePackages.dolphin
-    ];
-
-    # hint electron apps to use Wayland
-    environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
-    # enable screensharing
-    xdg.portal = {
-      enable = true;
-      extraPortals = with pkgs; [ xdg-desktop-portal-hyprland ];
-    };
+  # enable screensharing
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-hyprland ];
   };
 }
